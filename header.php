7421970118bce303a4b1e5372d2775a81ea03147
Jorge Luis Mayorga <jl.mayorga236@gmail.com>
2023-04-04 12:05:18 -0500

    V2

--- header.php
+++ header.php
@@ -1,50 +1,59 @@
 <?php
 /**
- * The header for our theme.
+ * The header for our theme
  *
- * Displays all of the <head> section and everything up till <div id="content">
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
- * @package storefront
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package BubbleSkinCareTheme
  */
 
-?><!doctype html>
-<html <?php language_attributes();?>>
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo('charset');?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo('pingback_url');?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-<?php wp_head();?>
+	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class();?>>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'bubbleskincare' ); ?></a>
 
-<?php wp_body_open();?>
+	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$bubbleskincare_description = get_bloginfo( 'description', 'display' );
+			if ( $bubbleskincare_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $bubbleskincare_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
 
-<?php do_action('storefront_before_site');?>
-
-<div id="page" class="hfeed site">
-	
-	<?php do_action('storefront_before_header');?>
-
-	<header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles();?>">
-		<?php include 'components/bsc_header.php';?>
-	</header>
-
-
-	<?php
-		/**
-		 * Functions hooked in to storefront_before_content
-		 *
-		 * @hooked storefront_header_widget_region - 10
-		 * @hooked woocommerce_breadcrumb - 10
-		 */
-		do_action('storefront_before_content');
-		?>
-
-		<div id="content" class="site-content" tabindex="-1">
-			<div class="col-full">
-
-		<?php
-			do_action('storefront_content_top');
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bubbleskincare' ); ?></button>
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				)
+			);
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
